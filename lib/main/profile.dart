74836d4d4ddd75b3d5d8e7c38071934c6baee486
Poyraz Erdoğan <poyraz01122004@gmail.com>
2022-02-21 19:25:02 +0300

    commit

--- lib/main/profile.dart
+++ lib/main/profile.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'lesson/lesson.dart';
+import 'dart:convert';
+import 'dart:async' show Future;
 
 class Profile extends StatefulWidget {
 
@@ -12,13 +16,24 @@ class _ProfileState extends State<Profile> {
     return buildBody(context);
   }
   
+  final lesson = Lesson();
+
+  late List data;
+
+  Future<String> loadJsonData() async {
+    var jsonText = await rootBundle.loadString('assets/people.json');
+    setState(() => data = json.decode(jsonText));
+    return 'success';
+  }
+
+  
   Widget buildBody(BuildContext context){
     return SingleChildScrollView(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         //crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Center(
+        children: [
+          const Center(
             child: Padding(
               padding: EdgeInsets.all(15.0),
               child: CircleAvatar(
@@ -31,7 +46,16 @@ class _ProfileState extends State<Profile> {
           Center(
             child: Padding(
               padding: EdgeInsets.all(15.0),
-              child: Text("Poyraz Erdoğan", textAlign: TextAlign.right, style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),),
+              //child: Text("Poyraz Erdoğan", textAlign: TextAlign.right, style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),),
+              child: ListView.builder(
+                itemCount: 2,
+                itemBuilder: (BuildContext context, int index){
+                  loadJsonData();
+                  return ListTile(
+                    title: data[index]["title"],
+                  );
+                },
+              )
             ),
           )
         ],
